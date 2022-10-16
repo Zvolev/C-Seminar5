d02c24ecd7101b58800684499f561358705fe71f
Evgenii <zvolev@mail.ru>
2022-10-16 23:44:51 +0300

    Task5 - свое решение на основе ренее созданных методах и содан новый по теме...

---
+++ Task5/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Найдите произведение пар чисел в одномерном массиве. 
+Парой считаем первый и последний элемент, второй и предпоследний 
+и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+Решение в группах задач:*/
+
+int InputArraySize(string message) // запрос, ввод  и проверка размера массива 
+{
+    Console.Write(message);
+    int length = Convert.ToInt32(Console.ReadLine());
+    if (length <= 0)
+        Console.Write("Недопустимый размер массива");
+    else
+        Console.WriteLine();
+    return length;
+}
+
+int Prompt(string message) // запрос и ввод значений
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] CreateRandomArray(int argLength = 2, int argMin = 0, int argMax = 1) // создаем массив и наполняем его случайными значениями по заданным параметрам и выводим на экран
+{
+    int[] array = new int[argLength];
+
+    Random rand = new Random();
+    for (int i = 0; i < argLength; i++)
+    {
+        array[i] = rand.Next(argMin, argMax);
+        //System.Console.WriteLine(array[i]);
+    }
+    return array;
+}
+
+int[] MultiplicationOfPairsArray(int[] array) //находит произведение пар чисел в массиве и записывает их в новый массив
+{
+    int len = array.Length;
+    int[] arr = new int[len / 2];
+    for (int i = 0; i < len / 2; i++)
+    {
+        arr[i] = array[i] * array[len - 1 - i];
+    }
+    return arr;
+}
+
+void PrintArray(int[] array) // выводим массив на печать
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+
+int len = InputArraySize("Укажите размер массива >- ");
+int min = Prompt("Укажите диапазон значений массива: от >- ");
+int max = Prompt("до (включительно) >- ");
+int[] array = CreateRandomArray(len, min, max - 1); // создаем массив по заданным параметрам
+PrintArray(array); // выводим массив на печать
+System.Console.WriteLine();
+PrintArray(MultiplicationOfPairsArray(array)); // выводим массив на печать
