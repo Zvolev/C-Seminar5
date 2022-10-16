afa51d7e4bd0017ec2cc819d8f8059e5b6a5239e
Evgenii <zvolev@mail.ru>
2022-10-16 22:52:33 +0300

    Task4 - свое решение на основе ренее созданных методах и содан новый по теме...

---
+++ Task4/Program.cs
@@ -0,0 +1,72 @@
+﻿/*Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежат в 
+отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 
+123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5*/
+
+int InputArraySize(string message) // запрос, ввод  и проверка размера массива 
+{
+    Console.Write(message);
+    int length = Convert.ToInt32(Console.ReadLine());
+    if (length <= 0)
+        Console.Write("Недопустимый размер массива");
+    else
+        Console.WriteLine();
+    return length;
+}
+
+
+int Prompt(string message) // запрос и ввод значений
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+
+int[] CreateRandomArray(int argLength = 2, int argMin = 0, int argMax = 1) // создаем массив и наполняем его случайными значениями по заданным параметрам и выводим на экран
+{
+    int[] array = new int[argLength];
+
+    Random rand = new Random();
+    for (int i = 0; i < argLength; i++)
+    {
+        array[i] = rand.Next(argMin, argMax);
+        //System.Console.WriteLine(array[i]);
+    }
+    return array;
+}
+
+int AmountArrayElements(int[] array, int argMin, int argMax) // 
+{
+    int amount = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= argMin && array[i] <= argMax)
+        {
+            amount++;
+        }
+    }
+    return amount;
+}
+
+void PrintArray(int[] array) // выводим массив на печать
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+
+int len = InputArraySize("Укажите размер массива >- ");
+int min = Prompt("Укажите диапазон значений массива: от >- ");
+int max = Prompt("до (включительно) >- ");
+int[] array = CreateRandomArray(len, min, max - 1); // создаем массив по заданным параметрам
+PrintArray(array); // выводим массив на печать
+System.Console.WriteLine();
+System.Console.WriteLine($"{AmountArrayElements(array, 10, 100)} элементов массива лежат в диапазоне от 10 до 99");
